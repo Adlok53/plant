264dc66636610e87fce921d1ed3b9a1a65323f1d
Adlok53 <01mikinini@gmail.com>
2024-09-17 23:51:41 +0300

    Создание логики игры

---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProjectTest
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
